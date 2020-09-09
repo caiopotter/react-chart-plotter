6636750bf6981e83e14cd47072b7f107d56e0a34
caiopotter <limapotter@gmail.com>
2020-09-09 18:02:50 -0300

    Adjust button placement

--- src/components/jsonEditor.js
+++ src/components/jsonEditor.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
-
+import { Button, Navbar } from 'react-bootstrap';
 
 function JSONEditor(props){
     const editor = (
-        <div><textarea className="text-area" value={props.json} onChange={props.onChange} />
-        <Button onClick={props.onClick}>Generate Chart</Button>
+        <div>
+            <div>
+                <textarea className="text-area" value={props.json} onChange={props.onChange} />
+            </div>
+            <div className="fixed-bottom">  
+            <Navbar className="footer">
+                    <Button onClick={props.onClick}>Generate Chart</Button>
+            </Navbar>
+            </div>
         </div>
     )
 
